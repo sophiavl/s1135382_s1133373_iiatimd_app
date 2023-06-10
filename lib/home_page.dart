f3e21eb6a3ee1f8189107a13682ef22f35736c8f
JelmerBankras <s1133373@student.hsleiden.nl>
2023-06-10 15:44:34 +0200

    wat shit gefixt

---
+++ lib/home_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'workout_page.dart';
+import 'package:intl/intl.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color background = const Color(0xFF1B1B1B);
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('EEEE').format(now);
+    return Container(
+      color: background, // Set the background color for the whole screen
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+              'Welcome, User',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.height * 0.2,
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(10),
+                  bottomLeft: Radius.circular(5),
+                  bottomRight: Radius.circular(15),
+                ),
+              ),
+              child: const Text('Hello'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (BuildContext context) {
+                    return const WorkoutPage();
+                  }),
+                );
+              },
+              child: Text(formattedDate),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
