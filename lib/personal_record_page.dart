14066ac5108c438ab3deaeceb70f3fe545779b49
sophiavl <102966872+sophiavl@users.noreply.github.com>
2023-06-16 09:49:18 +0200

    added new page file for personal records page

---
+++ lib/personal_record_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PersonalRecord extends StatefulWidget {
+  const PersonalRecord({super.key});
+
+  @override
+  State<PersonalRecord> createState() => _PersonalRecordState();
+}
+
+class _PersonalRecordState extends State<PersonalRecord> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
