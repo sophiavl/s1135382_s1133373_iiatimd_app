a93c8405c74641a53955f00a2fb6e4314b8ce51f
sophiavl <102966872+sophiavl@users.noreply.github.com>
2023-06-25 12:18:39 +0200

    Made a model for adding a workout sort

--- lib/abs_page.dart
+++ lib/abs_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:s1135382_s1133373_iiatimd_app/components/navbar.dart';
+import 'package:s1135382_s1133373_iiatimd_app/components/workout_sort.dart';
 
 class Abs extends StatefulWidget {
   const Abs({super.key});
@@ -12,6 +12,7 @@ class _AbsState extends State<Abs> {
   Color backgroundContainer = const Color(0xFF5D2E6D);
   Color defaultColor = const Color(0xFF9F51BA);
   Color background = const Color(0xFF1B1B1B);
+  Color text = const Color(0xFFFDFDFD);
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +22,13 @@ class _AbsState extends State<Abs> {
         centerTitle: true,
       ),
       backgroundColor: background,
-      body: const SingleChildScrollView(
-        child: Text('Abs'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            WorkoutSort(workoutName: 'Plank'),
+            WorkoutSort(workoutName: 'Crunches')
+          ],
+        ),
       ),
     );
   }
